b6854815e9bd61b6019fb475dcf2dd6a4ca14c63
Edward Lale <edward.gratefuldead1@gmail.com>
2025-12-02 14:54:35 -0600

    Refactor SpeedInsights integration to use React component

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { injectSpeedInsights } from "@vercel/speed-insights";
+import { SpeedInsights } from "@vercel/speed-insights/react"; 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    {injectSpeedInsights()}
+    <SpeedInsights />
   </StrictMode>
 );
